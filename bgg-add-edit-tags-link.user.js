17f67ddb3aa9d620fd8f6d42e0a79f4b09031ab4
kofaysi <17022173+kofaysi@users.noreply.github.com>
2024-10-20 21:54:46 +0200

    Update bgg-add-edit-tags-link.user.js

--- bgg-add-edit-tags-link.user.js
+++ bgg-add-edit-tags-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add Edit Tags Link
-// @namespace    https://github.com/kofaysi/
-// @version      2024-07-24
+// @namespace    https://github.com/kofaysi/bgg-userscripts/blob/main/bgg-add-edit-tags-link.user.js
+// @version      2.1
 // @description  Add an "Edit Tags" link to each div with the class 'tagouter' aligned to the right within the row of the board game name
 // @author       https://github.com/kofaysi/
 // @match        https://boardgamegeek.com/tag/*
